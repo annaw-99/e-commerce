6c433df155ec0a5d9ae8be9da5defd65b33c2fe9
Tung-Yen Wang <annawang9909@gmail.com>
2025-05-24 23:55:13 -0400

    added search function

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -8,8 +8,11 @@ import { FiltersDialogComponent } from './filters-dialog/filters-dialog.componen
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 import { ShopParams } from '../../shared/models/shopParams';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
@@ -22,7 +25,9 @@ import { ShopParams } from '../../shared/models/shopParams';
     MatMenu,
     MatMenuTrigger,
     MatSelectionList,
-    MatListOption
+    MatListOption,
+    MatPaginator,
+    FormsModule
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -31,7 +36,7 @@ export class ShopComponent implements OnInit{
   private shopService = inject(ShopService);
   private dialogService = inject(MatDialog);
   title = 'client';
-  products: Product[] = [];
+  products?: Pagination<Product>;
   sortOptions = [
     {name: 'Alphabetical', value: 'name'},
     {name: 'Price: Low-High', value: 'priceAsc'},
@@ -39,6 +44,7 @@ export class ShopComponent implements OnInit{
   ]
 
   shopParams = new ShopParams();
+  pageSizeOptions = [5,10,15,20];
 
   ngOnInit(): void {
     this.intitializeShop();
@@ -52,15 +58,27 @@ export class ShopComponent implements OnInit{
 
   getProducts() {
     this.shopService.getProducts(this.shopParams).subscribe({
-      next: response => this.products = response.data,
+      next: response => this.products = response,
       error: error => console.log(error)
     })
+  }
+
+  onSearchChange() {
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.shopParams.pageNumber = event.pageIndex + 1;
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
   }
 
   onSortChange(event: MatSelectionListChange) {
     const selectedOption = event.options[0];
     if (selectedOption) {
       this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1;
       this.getProducts();
     }
   }
@@ -78,6 +96,7 @@ export class ShopComponent implements OnInit{
         if (result){
           this.shopParams.brands = result.selectedBrands;
           this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1;
           this.getProducts();
         }
       }
