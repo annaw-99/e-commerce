fa3177f25cd9290390cd4f6b4be08670f73727a3
Tung-Yen Wang <annawang9909@gmail.com>
2025-05-25 17:51:31 -0400

    added product details page

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { MatBadge } from '@angular/material/badge';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
@@ -10,7 +11,9 @@ import { MatBadge } from '@angular/material/badge';
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
