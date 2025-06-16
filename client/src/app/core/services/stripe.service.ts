0e9d6c79690245844f21d82158b9dfd4c1a353ef
Tung-Yen Wang <annawang9909@gmail.com>
2025-06-16 17:44:50 -0400

    updated delivery component

--- client/src/app/core/services/stripe.service.ts
+++ client/src/app/core/services/stripe.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { loadStripe, Stripe, StripeElement, StripeElements } from '@stripe/stripe-js';
+import { loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElement, StripeElements } from '@stripe/stripe-js';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CartService } from './cart.service';
 import { Cart } from '../../shared/models/cart';
 import { firstValueFrom, map } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,11 @@ import { firstValueFrom, map } from 'rxjs';
 export class StripeService {
   baseUrl = environment.apiUrl;
   private cartService = inject(CartService);
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   private stripePromise: Promise<Stripe | null>;
   private elements?: StripeElements;
+  private addressElement?: StripeAddressElement;
 
   constructor() {
     this.stripePromise = loadStripe(environment.stripePublicKey)
@@ -37,6 +40,40 @@ export class StripeService {
     return this.elements;
   }
 
+  async createAddressElement() {
+    if (!this.addressElement) {
+      const elements = await this.initializeElements();
+      if (elements) {
+        const user = this.accountService.currentUser();
+        let defaultValues: StripeAddressElementOptions['defaultValues'] = {};
+
+        if (user) {
+          defaultValues.name = user.firstName + ' ' + user.lastName;
+        }
+
+        if (user?.address) {
+          defaultValues.address = {
+            line1: user.address.line1,
+            line2: user.address.line2,
+            city: user.address.city,
+            state: user.address.state,
+            country: user.address.country,
+            postal_code: user.address.postalCode
+          }
+        }
+
+        const options: StripeAddressElementOptions = {
+          mode: 'shipping',
+          defaultValues
+        };
+        this.addressElement = elements.create('address', options);
+      } else {
+        throw new Error('Elements instance has not been loaded');
+      }
+    }
+    return this.addressElement;
+  }
+
   createOrUpdatePaymentIntent() {
     const cart = this.cartService.cart();
     if (!cart) throw new Error("Problem with cart");
@@ -46,5 +83,10 @@ export class StripeService {
         return cart;
       })
     )
+  }
+
+  disposeElements() {
+    this.elements = undefined;
+    this.addressElement = undefined;
   }
 }
---
+++ client/src/app/features/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CheckoutService } from '../../../core/services/checkout.service';
+import { MatRadioModule } from '@angular/material/radio'
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout-delivery',
+  standalone: true,
+  imports: [
+    MatRadioModule,
+    CurrencyPipe
+  ],
+  templateUrl: './checkout-delivery.component.html',
+  styleUrl: './checkout-delivery.component.scss'
+})
+export class CheckoutDeliveryComponent implements OnInit{
+  checkoutService = inject(CheckoutService);
+
+  ngOnInit(): void {
+      this.checkoutService.getDeliveryMethods().subscribe();
+  }
+}
