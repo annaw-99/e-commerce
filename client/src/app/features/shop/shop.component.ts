65e2a6ef1aef662887248511b0e2983b1eafa825
Tung-Yen Wang <annawang9909@gmail.com>
2025-07-09 21:54:16 -0400

    ci: angular app build

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -13,6 +13,7 @@ import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angula
 import { ShopParams } from '../../shared/models/shopParams';
 import { Pagination } from '../../shared/models/pagination';
 import { FormsModule } from '@angular/forms';
+import { EmptyStateComponent } from "../../shared/components/empty-state/empty-state.component";
 
 @Component({
   selector: 'app-shop',
@@ -27,7 +28,8 @@ import { FormsModule } from '@angular/forms';
     MatSelectionList,
     MatListOption,
     MatPaginator,
-    FormsModule
+    FormsModule,
+    EmptyStateComponent
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -53,6 +55,11 @@ export class ShopComponent implements OnInit{
   intitializeShop() {
     this.shopService.getBrands();
     this.shopService.getTypes();
+    this.getProducts();
+  }
+
+  resetFilters() {
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
 
